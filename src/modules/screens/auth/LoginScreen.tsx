aa928ba2240ab2801d8fd100ed1a3b097d955e90
Syed Umer Ali <sumer@digital-dividend.com>
2025-07-08 23:31:21 +0500

    Validations for empty email

--- src/modules/screens/auth/LoginScreen.tsx
+++ src/modules/screens/auth/LoginScreen.tsx
@@ -5,8 +5,14 @@ import TextInput from "@/components/TextInput";
 import TouchableText from "@/components/TouchableText";
 import useCustomAnimation from "@/hooks/useCustomAnimation";
 import { BlurView } from "expo-blur";
-import React, { useState } from "react";
-import { Alert, Animated, ImageBackground, View } from "react-native";
+import React, { useRef, useState } from "react";
+import {
+  Alert,
+  Animated,
+  ImageBackground,
+  TextInput as TextInputRN,
+  View,
+} from "react-native";
 import Helper from "../../../utils/Helper";
 import styles from "./styles";
 
@@ -15,6 +21,8 @@ const LoginScreen = () => {
     email: "",
     password: "",
   });
+  const emailRef = useRef<TextInputRN>(null);
+  const passwordRef = useRef<TextInputRN>(null);
 
   const { buttonOpacity, buttonTranslateY, heightAnim } =
     useCustomAnimation.useAnimateButton(
@@ -28,12 +36,23 @@ const LoginScreen = () => {
   };
 
   const onSubmit = () => {
+    const emptyKeys = Object.keys(credentials).filter(
+      (key) => credentials[key] === ""
+    );
+
+    if (emptyKeys?.length > 0) {
+      Alert.alert(`${emptyKeys?.[0]} is missing`);
+      return;
+    }
+
     if (!Helper.isEmailValid(credentials.email)) {
       Alert.alert("Incorrect Email Address");
+      return;
     }
 
     if (!Helper.isPasswordValid(credentials.password)) {
       Alert.alert("Incorrect Password");
+      return;
     }
   };
 
@@ -56,12 +75,16 @@ const LoginScreen = () => {
             placeholder="Enter email"
             value={credentials.email}
             onChangeText={(text) => handleChange("email", text)}
+            ref={emailRef}
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
           <TextInput
             label="Password"
             placeholder="Enter password"
             value={credentials.password}
             onChangeText={(text) => handleChange("password", text)}
+            ref={passwordRef}
+            onSubmitEditing={onSubmit}
           />
 
           <Animated.View
