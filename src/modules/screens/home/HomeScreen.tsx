d7113e0119a1572b526d0984fffffa52249d4aa3
Syed Umer Ali <sumer@digital-dividend.com>
2025-07-10 00:42:11 +0500

    Fade animation for background of home is in process

--- src/modules/screens/home/HomeScreen.tsx
+++ src/modules/screens/home/HomeScreen.tsx
@@ -1,10 +1,10 @@
-import images from "@/assets/images";
 import FollowCard from "@/components/Card/FollowCard";
 import Text from "@/components/Text";
 import { dummyData } from "@/constants/DummyData";
 import { BlurView } from "expo-blur";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import {
+  Animated,
   FlatList,
   ImageBackground,
   ListRenderItemInfo,
@@ -15,11 +15,33 @@ import { FollowItem } from "./types";
 
 const HomeScreen = () => {
   const [activeSlide, setActiveSlide] = useState(null);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const onViewRef = useRef(({ viewableItems }) => {
+    if (viewableItems?.length > 0) {
+      const centeredItem = viewableItems[0]?.item;
+      if (centeredItem?.id !== activeSlide?.id) {
+        setActiveSlide(centeredItem);
+        Animated.timing(fadeAnim, {
+          toValue: 1,
+          duration: 400,
+          useNativeDriver: false,
+        }).start(() => {
+          fadeAnim.setValue(0);
+        });
+      }
+    }
+  });
+
+  const viewConfigRef = useRef({
+    itemVisiblePercentThreshold: 60,
+    minimumViewTime: 100,
+  });
+
   const renderItem = ({ item, index }: ListRenderItemInfo<FollowItem>) => {
     return <FollowCard item={item} index={index} />;
   };
   return (
-    <ImageBackground style={styles.mainContainer} source={images.model1}>
+    <ImageBackground style={styles.mainContainer} source={activeSlide?.image}>
       <BlurView intensity={20} style={styles.blurContainer}>
         <View style={styles.overlay} />
 
@@ -35,6 +57,9 @@ const HomeScreen = () => {
           renderItem={renderItem}
           horizontal={true}
           keyExtractor={(item) => item.id}
+          onViewableItemsChanged={onViewRef.current}
+          viewabilityConfig={viewConfigRef.current}
+          showsHorizontalScrollIndicator={false}
         />
       </BlurView>
     </ImageBackground>
